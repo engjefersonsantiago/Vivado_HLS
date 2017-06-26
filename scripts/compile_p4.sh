8e53bc99127a5f714aa8877ba0ebf335dfa831fb
Jeferson Santiago da Silva <eng.jefersonsantiago@gmail.com>
2017-06-26 09:18:07 -0400

    Fixed P4 compilation

--- scripts/compile_p4.sh
+++ scripts/compile_p4.sh
@@ -1,7 +1,9 @@
 
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-source $THIS_DIR/../../env.sh
+#source $THIS_DIR/../../env.sh
+
+P4C_BM_PATH=$THIS_DIR/../../P4/p4c-bmv2
 
 P4C_BM_SCRIPT=$P4C_BM_PATH/p4c_bm/__main__.py
 
@@ -14,7 +16,7 @@ CLI_PATH=$BMV2_PATH/targets/simple_switch/sswitch_CLI
 # minutes to give it time to start, then add the entries and put the switch
 # process back in the foreground
 set -m
-$P4C_BM_SCRIPT headers_and_parser.p4 --json headers_and_parser.json --primitives proprietary_primitives.json  --p4-v1.1 
+$P4C_BM_SCRIPT headers_and_parser.p4 --json headers_and_parser.json  --p4-v1.1 
 if [ $? -ne 0 ]; then
 echo "p4 compilation failed"
 exit 1
